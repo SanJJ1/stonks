d51ecb15c23ccada22bbae92c128e66491848c72
Shyamsaibethina <44377581+Shyamsaibethina@users.noreply.github.com>
2020-08-02 13:46:46 -0400

    commit message

--- alphavantagetest/alpha-vantage-2.20.py
+++ alphavantagetest/alpha-vantage-2.20.py
@@ -1,8 +1,22 @@
 from alpha_vantage.timeseries import TimeSeries
 from pprint import pprint
+import matplotlib.pyplot as plt
+from alpha_vantage.techindicators import TechIndicators
 
 #getting stock info
-ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas')
+ts = TimeSeries(key='CE5874ROQF7V2C3N', output_format='pandas')
 data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
-pprint(data.head(2))
+pprint(data.head(4))
 
+#Graphing stock data
+data['4. close'].plot()
+plt.title('Intraday Times Series for the MSFT stock (1 min)')
+
+
+#Technical indicators- Bollinger Bands
+ti = TechIndicators(key='CE5874ROQF7V2C3N', output_format='pandas')
+data1, meta_data1 = ti.get_bbands(symbol='MSFT', interval='60min', time_period=60)
+data1.plot()
+plt.title('BBbands indicator for  MSFT stock (60 min)')
+
+plt.show()
